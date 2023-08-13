6000e0997e0b43b67b86804df4667ed809a4bfa2
Chitaolang <103441935+chitaolang@users.noreply.github.com>
2023-08-13 13:40:56 +0800

    Create useMint.ts
    add mint hook

---
+++ frontend/hooks/useMint.ts
@@ -0,0 +1,21 @@
+import { usePrepareContractWrite, useContractWrite, useWaitForTransaction } from "wagmi";
+import nftABI from "@/abi/NFT";
+import { NftAddress } from "@/constants/Address"
+
+const useMint = (influencer: `0x${string}`, campaignId: number)=> {
+    const { config } = usePrepareContractWrite({
+        address: NftAddress,
+        abi: nftABI,
+        functionName: 'mint',
+        args: [influencer, campaignId]
+    });
+
+    const { write, data } = useContractWrite(config);
+    const { isLoading, isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+    })
+
+    return { write, isLoading, isSuccess, data}
+}
+
+export default useMint;
