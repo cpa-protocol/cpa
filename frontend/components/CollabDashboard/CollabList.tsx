802014aab6d4d714f36a8ae53caa7f9767eb15bf
akirawuc <stu970029@gmail.com>
2023-08-13 21:40:22 +0800

    wip: set up nft issue

--- frontend/components/CollabDashboard/CollabList.tsx
+++ frontend/components/CollabDashboard/CollabList.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import {formatEther} from "viem";
 import useGetAllCampaigns from "@/hooks/useGetAllCampaigns";
+import useInfluencersNftContract from "@/hooks/useInfluencersNftContract";
 import { useAccount } from "wagmi";
 import { useRouter } from 'next/router';
 import CollabDetail from './CollabDetailPopup';
@@ -10,15 +11,32 @@ import Link from 'next/link';
 import * as z from "zod";
 import { useFieldArray, useForm } from "react-hook-form";
 import { Button } from "../ui/button";
+import useSetUpNFT from '@/hooks/useSetUpNFT';
 
+const IssueCollabNFT = (campaign) => {
+    console.log(campaign);
+    const { write, isLoading, isSuccess } = useSetUpNFT(campaign.id.toNumber(), campaign.name, 'testurl');
+    const onSubmit = () => {
+        if (write) {
+            write();
+        }
+    }
+  return (
+    <div>
+      <Button onClick={onSubmit}>Issue Collab NFT</Button>
+    </div>
+  );
+};
 
 
 function CollabList({ blocks }) {
+  const { address, connector, isConnected } = useAccount();
   const [showDetails, setShowDetails] = useState(false);
   const [selectedCollab, setSelectedCollab] = useState(null);
   const {data : campaigns, isLoading, isSuccess} = useGetAllCampaigns();
   console.log(`campaigns are ${campaigns}`);
 
+  // console.log(useInfluencersNftContract(address, 1).data[2]);
   const handleViewDetails = (collab) => {
     setSelectedCollab(collab);
     console.log(collab);
@@ -34,9 +52,9 @@ function CollabList({ blocks }) {
             <p className="text-sm text-gray-600"> Quota: {`${block.audience}`}</p>
             <p className="text-sm text-gray-600 mb-10"> Reward per action : {formatEther(`${block.cpa}`)} ETH</p>
             {
-              (block.Status == "Active") ? (
+                (useInfluencersNftContract(address, Number(block.id)).data[2]=="0x0000000000000000000000000000000000000000") ? (
                 <div>
-                  <Button>Accept</Button>
+                    <IssueCollabNFT campaign={block}/>
                 </div>
               ) : (
                 <div>
@@ -54,6 +72,7 @@ function CollabList({ blocks }) {
 
 function CollabDashboard() {
   const {data : campaigns, isLoading, isSuccess} = useGetAllCampaigns();
+    console.log(campaigns);
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             {isSuccess && <CollabList blocks={campaigns} />}
