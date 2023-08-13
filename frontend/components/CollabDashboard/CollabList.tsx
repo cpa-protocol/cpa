a7e3b5ba1a6de4e6b10423dd0ea9b5b66d96dcfb
akirawuc <stu970029@gmail.com>
2023-08-13 15:30:23 +0800

    feat: read from contract and display campaign details as block

--- frontend/components/CollabDashboard/CollabList.tsx
+++ frontend/components/CollabDashboard/CollabList.tsx
@@ -1,4 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import {formatEther} from "viem";
+import useGetAllCampaigns from "@/hooks/useGetAllCampaigns";
 import { useAccount } from "wagmi";
 import { useRouter } from 'next/router';
 import CollabDetail from './CollabDetailPopup';
@@ -9,10 +11,24 @@ import * as z from "zod";
 import { useFieldArray, useForm } from "react-hook-form";
 import { Button } from "../ui/button";
 
+function CollabDashboard() {
+  const {data : campaigns, isLoading, isSuccess} = useGetAllCampaigns();
+    if (isSuccess) {
+        console.log('campaigns are');
+        console.log(campaigns[0]);
+    };
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            {isSuccess && <CollabList blocks={campaigns} />}
+        </div>
+    )
+}
 
 function CollabList({ blocks }) {
   const [showDetails, setShowDetails] = useState(false);
   const [selectedCollab, setSelectedCollab] = useState(null);
+  const {data : campaigns, isLoading, isSuccess} = useGetAllCampaigns();
+  console.log(`campaigns are ${campaigns}`);
 
   const handleViewDetails = (collab) => {
     setSelectedCollab(collab);
@@ -25,9 +41,10 @@ function CollabList({ blocks }) {
       <div className="grid grid-cols-3 gap-4">
         {blocks.map((block, index) => (
           <div key={index} className="border p-4 rounded-3xl">
-            <h3 className="font-bold">{block.Title}</h3>
-            <p className="text-sm text-gray-600"> Quota: {block.RemainQuota}</p>
-            <p className="text-sm text-gray-600 mb-10"> Reward per action : {block.RewardPerAction} ETH</p>
+            <h3 className="font-bold">{block.name}</h3>
+            {console.log(typeof block.audience)}
+            <p className="text-sm text-gray-600"> Quota: {`${block.audience}`}</p>
+            <p className="text-sm text-gray-600 mb-10"> Reward per action : {formatEther(`${block.cpa}`)} ETH</p>
             {
               (block.Status == "Active") ? (
                 <div>
@@ -47,6 +64,6 @@ function CollabList({ blocks }) {
   );
 }
 
-export default CollabList;
+export default CollabDashboard;
 
 
