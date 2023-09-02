f8c59a5058a3e51835e96dcad8577974f8f7e3c5
akirawuc <stu970029@gmail.com>
2023-09-02 17:50:21 +0800

    feat: protocol page style & header fading

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
 
 function Header() {
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let scrollTop = window.scrollY;
+      let newOpacity = 1 - scrollTop / 300;
+      if (newOpacity < 0) newOpacity = 0;
+      if (newOpacity > 1) newOpacity = 1;
+      setOpacity(newOpacity);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="sticky top-0 z-40">
+    <header className="sticky top-0 z-[1000]" style={{ opacity: opacity }}>
       <nav className="container flex w-full items-center justify-between py-2 mx-auto">
         <Link href="/">
           <div className="flex justify-center content-center items-center">
