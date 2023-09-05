95cfb5247489f827901e4c6e091c7ccd8747ed3b
akirawuc <stu970029@gmail.com>
2023-09-05 10:37:42 +0800

    feat: update config

--- frontend/wagmi.config.ts
+++ frontend/wagmi.config.ts
@@ -1,8 +1,8 @@
 // @ts-nocheck
 import { defineConfig } from "@wagmi/cli";
 import { react } from "@wagmi/cli/plugins";
-import nftABI from "@/abi/NFT";
-import cpaABI from "@/abi/NFT";
+import NFTABI from "@/abi/NFT";
+import CPAABI from "@/abi/NFT";
 import { erc20ABI } from "wagmi";
 import { 
   // optimism,
@@ -18,7 +18,7 @@ export default defineConfig({
   contracts: [
     {
       name: "cpa",
-      abi: cpaABI,
+      abi: CPAABI,
       address: {
           [optimismGoerli.id]: '0x8cAf9989e73491b60a923a0eB3Ce1436A4604752',
           [baseGoerli.id]: '0xe64154515166F86ce77c47B35dE3E6F0cCe4cBb5',
@@ -26,12 +26,14 @@ export default defineConfig({
     },
     {
       name: "nft",
-      abi: nftABI,
+      abi: NFTABI,
       address: {
           [optimismGoerli.id]: '0x9e55B1C55Bb47fc58Badab6610B3445101c051dC',
           [baseGoerli.id]: '0x47625465f936920Efa00e33391125fCcfA106d84',
       },
     },
   ],
-  plugins: [react()],
+    plugins: [react({
+        usePrepareContractFunctionWrite: true,
+    })],
 });
